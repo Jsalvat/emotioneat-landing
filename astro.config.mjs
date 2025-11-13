946f28137fa11bff0086d38307949e018cc34711
EmotionEat <jsalvat87@gmail.com>
2025-11-13 13:42:37 +0100

    WS

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
-import netlify from '@astrojs/netlify';
+import netlify from '@astrojs/netlify/functions'; // <--- usar /functions para SSR
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://emotioneat.com',
   integrations: [
@@ -25,7 +24,6 @@ export default defineConfig({
       prefixDefaultLocale: false
     }
   },
+  output: 'server', // <--- importante para SSR
   adapter: netlify()
 });
-
-
